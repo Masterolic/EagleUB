e6dc8d701219078537b51d384340fa3dd2693eb5
Masterolic <93469093+Masterolic@users.noreply.github.com>
2021-11-28 09:01:32 +0530

    Update inline_utils.py

--- assistant/inline_utils.py
+++ assistant/inline_utils.py
@@ -237,27 +237,27 @@ async def owo(client, inline_query):
         bttn = [
             [
                 InlineKeyboardButton(
-                    text="Command Help", callback_data=f"make_cmd_buttons"
+                    text="Command Help😁", callback_data=f"make_cmd_buttons"
                 )
             ],
             [
              InlineKeyboardButton(
-                    text="Restart UserBot", callback_data=f"restart_bot"
+                    text="Restart 😴UserBot", callback_data=f"restart_bot"
                 )
             ],  
             [
              InlineKeyboardButton(
-                    text="Update UserBot", callback_data=f"updTe_bot"
+                    text="Update UserBot🤓", callback_data=f"updTe_bot"
                 )
             ],
             [
              InlineKeyboardButton(
-                    text="SyS Info", callback_data=f"sys_info"
+                    text="SyS Info🦅", callback_data=f"sys_info"
                 )
             ],
             [
              InlineKeyboardButton(
-                    text="Change UserBot Language", callback_data=f"change_lang"
+                    text="Change UserBot Language😇", callback_data=f"change_lang"
                 )
             ],
         ]
@@ -383,27 +383,27 @@ async def black_menu(client, cb):
     bttn = [
             [
                 InlineKeyboardButton(
-                    text="Command Help", callback_data=f"make_cmd_buttons"
+                    text="Command Help 😁", callback_data=f"make_cmd_buttons"
                 )
             ],
             [
              InlineKeyboardButton(
-                    text="Restart UserBot", callback_data=f"restart_bot"
+                    text="Restart UserBot😴", callback_data=f"restart_bot"
                 )
             ],  
             [
              InlineKeyboardButton(
-                    text="Update UserBot", callback_data=f"updTe_bot"
+                    text="Update UserBot🤓", callback_data=f"updTe_bot"
                 )
             ],
             [
              InlineKeyboardButton(
-                    text="SyS Info", callback_data=f"sys_info"
+                    text="SyS Info🦅", callback_data=f"sys_info"
                 )
             ],
         [
              InlineKeyboardButton(
-                    text="Change UserBot Language", callback_data=f"change_lang"
+                    text="Change UserBot Language😇", callback_data=f"change_lang"
                 )
             ],
         ]
